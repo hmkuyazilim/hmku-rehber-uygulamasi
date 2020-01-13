b211899faca2879bba887c1163af1ca0f9aaec1a
hmkuyazilim <hmkuyazilim@gmail.com>
2020-01-13 09:46:12 +0300

    css tasarlandı ve mobil için temel oluşturuldu

---
+++ app.js
@@ -0,0 +1,26 @@
+const title=document.querySelectorAll('.title');
+const mail=document.querySelectorAll('.mail');
+
+//console.log(Array.from(title));
+
+let titleArray=Array.from(title);
+
+titleArray.forEach((t)=>{
+
+   console.log(t.innerHTML.length);
+   if(t.innerHTML.length>25){
+    t.classList.remove('title');
+    t.classList.add('titleSmall');
+   }
+});
+
+let mailArray=Array.from(mail);
+mailArray.forEach((m)=>{
+
+    console.log(m.innerHTML.length);
+    if(m.innerHTML.length>80){
+     m.classList.remove('mail');
+     m.classList.add('mailSmall');
+    }
+ });
+
