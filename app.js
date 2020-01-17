c893d56b61ac1c8bb79242859bd6ed8c9d689227
hmkuyazilim <hmkuyazilim@gmail.com>
2020-01-17 16:40:43 +0300

    service worker oluşturuldu

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+if('serviceWorker' in navigator){
+   navigator.serviceWorker.register('/sw.js')
+   .then(reg=>console.log('service worker çalışıyor'))
+   .catch(err=>console.log('service worker çalışmıyor',err))
+}
+
+
 const title=document.querySelectorAll('.title');
 const mail=document.querySelectorAll('.mail');
 const input=document.querySelectorAll('.search_input');
@@ -8,7 +15,6 @@ let titleArray=Array.from(title);
 
 titleArray.forEach((t)=>{
 
-   console.log(t.innerHTML.length);
    if(t.innerHTML.length>25){
     t.classList.remove('title');
     t.classList.add('titleSmall');
@@ -18,7 +24,6 @@ titleArray.forEach((t)=>{
 let mailArray=Array.from(mail);
 mailArray.forEach((m)=>{
 
-    console.log(m.innerHTML.length);
     if(m.innerHTML.length>80){
      m.classList.remove('mail');
      m.classList.add('mailSmall');
---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install',event=>{
+
+    //console.log('service worker install oldu');
+});
+
+
+self.addEventListener('activate',event=>{
+
+    //console.log('service worker activate oldu');
+});
+
+self.addEventListener('fetch',event=>{
+
+    //console.log('service worker fetch oldu');
+});
